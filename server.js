de9ca6ab03086ed8b1d2587ca03869c727029d35
Aman Upadhyay <amanupadhyay33822@gmail.com>
2024-11-12 13:12:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use('/api', productRoutes);
+app.use('/product', productRoutes);
 
 app.get("/", (req, res) => {
   return res.send("Hi Everyone.");
