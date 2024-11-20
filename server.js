7ee14e3225e292ed376f6cac04c25a944c50f580
sharanQB <sharan.gopal@quadbtech.com>
2024-11-20 06:17:23 +0000

    add .env

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+const PORT = process.env.PORT || 8000;
 const productRoutes = require('./Routes/product'); 
 // * Middleware
 app.use(express.json());
