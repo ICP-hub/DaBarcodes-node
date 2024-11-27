64a87ad282cc603ef2da362d39b3ba81b2b3423c
Aman Upadhyay <amanupadhyay33822@gmail.com>
2024-11-27 11:40:45 +0530

    update cors

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 const PORT = process.env.PORT || 8000;
-const productRoutes = require('./Routes/product'); 
+const productRoutes = require("./Routes/product");
 // * Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
-app.use('/product', productRoutes);
+app.use("/product", productRoutes);
 
 app.get("/", (req, res) => {
   return res.send("Hi Everyone.");
 });
-
-
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
