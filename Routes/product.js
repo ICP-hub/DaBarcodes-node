9675349a17bf7386f37d40ed9d49dd21132cd671
Amanup984 <aman.upadhyay@quadbtech.com>
2024-12-11 22:54:08 +0530

    Update product.js

--- Routes/product.js
+++ Routes/product.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const productController = require('../Controllers/Product');  // adjust path if needed
 const router = express.Router();
+const multer = require("multer");
+const storage = multer.memoryStorage(); // Use memory storage for multer
+const upload = multer({ storage });
 
-
-router.post('/create',productController.createProduct);
-router.post('/all', productController.createAllProducts);
+router.post('/create',upload.single("image"),productController.createProduct);
+router.post('/all',upload.array('images'), productController.createMultipleProducts);
 router.get('/get', productController.getProducts);
 router.get('/get/:principleId', productController.getProductByPrincipleId);
 router.put('/updates/:id', productController.updateProduct);
